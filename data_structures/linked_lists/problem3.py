66f64a9932b952491214b8e753a63c5187c986b5
takacchi <uchiha.takahiro@gmail.com>
2019-06-07 23:18:47 +0200

    Added two more solution for linked list

---
+++ data_structures/linked_lists/problem3.py
@@ -0,0 +1,71 @@
+"""
+Given a linked list, rearrange the node values such that they appear in alternating
+low -> high -> low -> high -> ... form.
+For example, given 1 -> 2 -> 3 -> 4 -> 5, you should return 1 -> 3 -> 2 -> 5 -> 4
+"""
+
+# keeping track of even elements
+def rearrange_nodes(node):
+	even = True
+	cur = node
+
+	while cur.next:
+		if cur.data > cur.next.data and even:
+			cur.data, cur.next.data = cur.next.data, cur.data
+
+		elif cur.data < cur.next.data and not even:
+			cur.data, cur.next.data = cur.next.data, cur.data
+
+		even = not even
+		cur = cur.next
+
+	return node
+
+# keeping current and prev node in memory
+def alternate(node):
+	prev = node
+	cur = node.next
+
+	while cur:
+		if prev.data > cur.data:
+			prev.data, cur.data = cur.data, prev.data
+		if not cur.next:
+			break
+		if cur.next.data > cur.data:
+			cur.next.data, cur.data = cur.data, cur.next.data
+
+		prev = cur.next
+		cur = cur.next.next
+
+	return node
+
+# Test Code
+class Node:
+	def __init__(self, data, next=None):
+		self.data = data
+		self.next = next
+
+def show_list(node):
+	while node is not None:
+		print(node.data, end=" -> ")
+		node = node.next		
+	print()
+
+
+def main():
+	root = Node(1, Node(2, Node(3, Node(4, Node(5)))))
+	root_2 = Node(2, Node(1, Node(3, Node(9, Node(2)))))
+
+	print("Base: ")
+	show_list(root)
+
+	print("\nRearrange (first function):")
+	root = rearrange_nodes(root)
+	show_list(root)
+
+	print("\nRearrange (second function):")
+	root_2 = alternate(root_2)
+	show_list(root_2)
+
+if __name__=='__main__':
+	main()
---
+++ data_structures/linked_lists/problem4.py
@@ -0,0 +1,47 @@
+"""
+Given two singly linked lists that intersect at some point, find the intersecting node.
+Assume the lists are non-cyclical
+For example, given A = 3 - > 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with
+value 8. In this example, assume node with the same value are the exact same node object.
+
+Do this in O(m+n) time (where m and n are the length of the lists) and constant space
+"""
+
+def size(node):
+	if not node:
+		return 0
+	return 1 + size(node.next)
+
+def find_intersect(node_1, node_2):
+	size_1, size_2 = size(node_1), size(node_2)
+	curr_1, curr_2 = node_1, node_2
+
+	if size_1 > size_2:
+		for _ in range(size_1 - size_2):
+			curr_1 = curr_1.next
+	else:
+		for _ in range(size_2 - size_1):
+			curr_2 = curr_2.next
+
+	while curr_1 != curr_2:
+		curr_1 = curr_1.next
+		curr_2 = curr_2.next
+
+	return curr_1
+
+
+class Node():
+	def __init__(self, data, next=None):
+		self.data = data
+		self.next = next
+
+def main():
+	#A = 3 - > 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10
+	equal = Node(8, Node(10))
+	A = Node(3, Node(7, equal))
+	B = Node(99, Node(1, equal))
+
+	assert (find_intersect(A, B)), equal
+
+if __name__=='__main__':
+	main()
